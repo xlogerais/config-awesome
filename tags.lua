1e2ee49fcc7f1f95650523b1c1ac85cdac09700e
Xavier Logerais (Numericable - Ingénierie Système) <xlogerais@ncnumericable.com>
2014-02-21 14:30:59 +0100

    Modified tags settings

--- tags.lua
+++ tags.lua
@@ -2,13 +2,20 @@
 
 tags = {
   names  = { "terms",    "web",       "mail",      "communication", "bureautique", "multimédia", "perso",     "autres"   },
-  layout = { layouts[2], layouts[10], layouts[10], layouts[2],      layouts[12],   layouts[1],   layouts[10], layouts[1] }
+  layout = { layouts[2], layouts[10], layouts[2], layouts[2],      layouts[12],   layouts[1],   layouts[10], layouts[1] }
 }
 
 -- Creates the tags
  for s = 1, screen.count() do
      tags[s] = awful.tag(tags.names, s, tags.layout)
  end
+
+-- Customize "terms" tag
+for s = 1, screen.count() do
+    awful.tag.setnmaster(1,tags[s][1])
+    awful.tag.setncol(1,tags[s][1])
+    awful.tag.setmwfact(0.6,tags[s][1])
+end
 
 -- Customize "communication" tag
 for s = 1, screen.count() do
@@ -20,10 +27,3 @@ end
 --	tags[s] = awful.tag({ "➊", "➋", "➌", "➍", "➎", "➏", "➐", "➑", "➒"  }, s, layouts[1]) -- these are boring but I like them.  
 --    --tags[s] = awful.tag({ "♨", "⌨", "⚡", "✉", "☕", "❁", "☃", "☄", "⚢" }, s, layouts[1])
 --end
-
--- Customize "terms" tag
-for s = 1, screen.count() do
-    awful.tag.setnmaster (1, tags[s][1])
-    awful.tag.setncol(2, tags[s][1])
-    awful.tag.setmwfact (0.5, tags[s][1])
-end
