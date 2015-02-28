43b23cba9bbbeab6bada3fb3673b1dcbef26c9fc
Xavier Logerais <xavier@logerais.com>
2015-02-28 10:32:46 +0100

    Modified apps.lua

--- apps.lua
+++ apps.lua
@@ -3,21 +3,21 @@
 terminal = "urxvt"
 terminal_cmd = terminal .. " -e "
 
-editor            = "scripts/launch-editor "
+editor         = "scripts/launch-editor "
 
-browser           = "scripts/launch-browser "
-mailer            = "scripts/launch-mailer "
+browser        = "scripts/launch-browser "
+mailer         = "scripts/launch-mailer "
 
-filebrowser = "rox"
+filebrowser    = "gentoo"
 
-messenger         = "scripts/launch-messenger "
+messenger      = "scripts/launch-messenger "
 
-calendar          = "gsimplecal"
-calculator        = "xcalc"
+calculator     = "xcalc"
+calendar       = "xcalendar"
 
-audiomute    = "amixer -q set Master,0 playback toggle"
-audiovolup   = "amixer -q set Master,0 5%+"
-audiovoldown = "amixer -q set Master,0 5%-"
+audiomute      = "amixer -q set Master,0 playback toggle"
+audiovolup     = "amixer -q set Master,0 5%+"
+audiovoldown   = "amixer -q set Master,0 5%-"
 
 audioplayer    = terminal .. " -name mocp -geometry 105x40 -e mocp"
 audioplay      = "mocp -p"
@@ -26,4 +26,4 @@ audiostop      = "mocp -s"
 audioprev      = "mocp -r"
 audionext      = "mocp -f"
 
-screenlocker = "i3lock -c 000000"
+screenlocker   = "i3lock -c 000000"
